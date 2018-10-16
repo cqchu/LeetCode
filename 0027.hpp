cb979dbc4d7e0f248c20645b8387e2cfb3d7d243
cq_chu <cq_chu@163.com>
2018-10-16 09:35:47 -0400

    complete problem 27

---
+++ 0027.hpp
@@ -0,0 +1,36 @@
+#ifndef __0027_H
+#define __0027_H
+
+/**********************************************************
+Description:
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+**********************************************************/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int originLen = nums.size();
+        if(originLen == 0)
+            return 0;
+
+        int rmCnt = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(val == nums[i])
+                rmCnt ++;
+            else
+                nums[i-rmCnt] = nums[i];
+        }
+        nums.resize(originLen-rmCnt);
+        return originLen - rmCnt;
+    }
+};
+
+#endif
+
