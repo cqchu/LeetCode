c765a52faa014da7849a977d9750ddeed9a367d3
cq_chu <cq_chu@163.com>
2018-10-16 09:23:22 -0400

    complete problem 26

---
+++ 0026.hpp
@@ -0,0 +1,37 @@
+#ifndef __0026_H
+#define __0026_H
+
+/**********************************************************
+Description:
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+**********************************************************/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+
+        int allLen = 1;
+        int pre = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(pre != nums[i])
+            {
+                nums[allLen] = nums[i];
+                allLen ++;
+                pre = nums[i];
+            }
+        }
+
+        nums.resize(allLen);
+        return allLen;
+    }
+};
+
+#endif
+
