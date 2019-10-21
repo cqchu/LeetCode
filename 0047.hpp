0b8ea6e5d650157446c408a37981c5e80dd0aa19
cq_chu <cq_chu@163.com>
2019-10-21 09:39:00 +0800

    complete problem 47

---
+++ 0047.hpp
@@ -0,0 +1,48 @@
+#ifndef __0046_H
+#define __0046_H
+
+/*************************************************
+Description:
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+*************************************************/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void recurse(int curPos, vector<int> nums, vector<vector<int>>& res) {
+        if(curPos == nums.size()-1) {
+            res.push_back(nums);
+        }
+        else {
+            set<int> visit;
+            for(int i=curPos; i<nums.size(); i++) {
+                if (visit.find(nums[i]) == visit.end()) {
+                    visit.insert(nums[i]);
+                    swap(nums[curPos], nums[i]);
+                    recurse(curPos+1, nums, res);
+                    swap(nums[curPos], nums[i]);
+                }
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> res;
+        recurse(0, nums, res);
+        return res;
+    }
+};
+
+
+#endif
