a7b38ee1e409549911ab631ad44e7eddcd5c48a3
cq_chu <cq_chu@163.com>
2019-10-23 10:24:37 +0800

    complete problem 59

---
+++ 0059.hpp
@@ -0,0 +1,54 @@
+#ifndef __0058_H
+#define __0058_H
+
+/*************************************************
+Description:
+Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+Example:
+Input: 3
+Output:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+*************************************************/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n));
+        int top=0, down=n, left=0, right=n;
+        int cnt = 1;
+        while(cnt <= n*n) {
+            for(int i=left; i<right; i++) {
+                res[top][i] = cnt++;
+            }
+            if ((++top) >= down)
+                break;
+            for (int i=top; i<down; i++) {
+                res[i][right-1] = cnt++;
+            }
+            if ((--right) <= left)
+                break;
+            for (int i=right-1; i>=left; i--) {
+                res[down-1][i] = cnt++;
+            }
+            if ((--down) <= top)
+                break;
+            for (int i=down-1; i>=top; i--) {
+                res[i][left] = cnt++;
+            }
+            if ((++left) >= right)
+                break;
+        }
+        return res;
+    }
+};
+
+
+#endif
