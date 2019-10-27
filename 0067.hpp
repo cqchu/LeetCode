0c637de51200d50a74738449716588a3352d7ad1
cq_chu <cq_chu@163.com>
2019-10-27 11:37:48 +0800

    complete problem 67

---
+++ 0067.hpp
@@ -0,0 +1,44 @@
+#ifndef __0067_H
+#define __0067_H
+
+/********************************************
+Description:
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+********************************************/
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int carry = 0, bitRes = 0, iA = 0, iB = 0;
+        string res(max(a.size(), b.size()), '0');
+        for (int i=1; i<=max(a.size(), b.size()); i++) {
+            iA = iB = 0;
+            if (a.size() >= i) {
+                iA = a[a.size()-i] - '0';
+            }
+            if (b.size() >= i) {
+                iB = b[b.size()-i] - '0';
+            }
+            bitRes = (iA + iB + carry) % 2;
+            carry = (iA + iB + carry) / 2;
+            res[res.size()-i] = bitRes + '0';
+        }
+        
+        if (carry == 1) {
+            char chCarry = carry + '0';
+            res = chCarry + res;
+        }
+        return res;
+    }
+};
+
+#endif
