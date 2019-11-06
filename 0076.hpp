e47065a3c6b139789082474c501f866ab0126575
cq_chu <cq_chu@163.com>
2019-11-06 12:23:11 +0800

    complete problem 76

---
+++ 0076.hpp
@@ -0,0 +1,43 @@
+#ifndef __0076_H
+#define __0076_H
+
+/*******************************************
+Description:
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+Example:
+    Input: S = "ADOBECODEBANC", T = "ABC"
+    Output: "BANC"
+
+Note:
+    If there is no such window in S that covers all characters in T, return the empty string "".
+If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
+*******************************************/
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> hist(128, 0);
+        for (char c : t) 
+            hist[c]++;
+                          
+        int remaining = t.length();
+        int left = 0, right = 0, minStart = 0, minLen = numeric_limits<int>::max();
+        while (right < s.length()){
+            if (--hist[s[right++]] >= 0) 
+                remaining--;
+            while (remaining == 0){
+                if (right - left < minLen) {
+                     minLen = right - left;
+                     minStart = left;
+                }
+                if (++hist[s[left++]] > 0) 
+                    remaining++;
+            }
+            
+        }        
+        return minLen < numeric_limits<int>::max() ? s.substr(minStart, minLen) : "";
+    }
+};
+
+#endif
--- readme.md
+++ readme.md
@@ -9,7 +9,9 @@
 * 0040, "First Missing Positive": 类似与Counting Sort, 都是借用Index表示一些信息</br>
 * 0042, "Trapping Rain Water": leetcode题解中提供了一种巧妙的做法</br>
 * 0044, "Wildcard Matching": 一个典型的动归题, 非常便于理解动归, 但题解中有双指针的方法</br>
-* 0049, "Group Anagrams": 两种方法, 一种需要排序,一种是Cnt编码, 编程珠玑里也有提到, 后者时间复杂度更低</br>
+* 0049, "Group Anagrams": 两种方法, 一种需要排序, 一种是Cnt编码, 编程珠玑里也有提到, 后者时间复杂度更低</br>
 * 0050, "Pow(x, n)": 里面降低时间复杂度的方法需要注意</br>
 * 0051, "Maximum Subarray": 同时可以用分治和动归两种方法来解, 动归需要注意把握状态变化</br>
 * 0062, "Unique Paths": 可以用递归解, 但是超时了, 所以用0050题的方法进行优化, 即使用DP, 同时这题中还用了2D DP->1D DP</br>
+* 0075, "Sort Colors": 类似于Count Sort, 然后第二种方法挺有意思的</br>
+* 0076, "Minimum Window Substring": 滑动窗口法, 注意左右移动指针的规则</br>
