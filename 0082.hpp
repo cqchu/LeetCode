1d7d1adc7473e91c660a3acb3e1365b792d72fbc
cqchu <cq_chu@163.com>
2019-12-23 17:03:15 +0800

    complete problem 83

---
+++ 0082.hpp
@@ -0,0 +1,70 @@
+#ifndef __0082_H
+#define __0082_H
+
+/******************************************************
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+Example 1:
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+
+Example 2:
+Input: 1->1->1->2->3
+Output: 2->3
+******************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL)
+            return head;
+        
+        ListNode *lastDicNode=NULL, *curNode=head;
+        bool duplicateFlag = false;
+        while(curNode->next != NULL) {
+            ListNode *nextNode = curNode->next;
+            if (curNode->val == curNode->next->val) {
+                if (lastDicNode != NULL)
+                    lastDicNode->next = curNode->next;
+                else 
+                    head = curNode->next;
+                delete curNode;
+                duplicateFlag = true;
+            }
+            else {
+                if (duplicateFlag == true) {
+                    if (lastDicNode != NULL)
+                        lastDicNode->next = curNode->next;
+                    else 
+                        head = curNode->next;
+                    delete curNode;
+                    duplicateFlag = false;
+                }
+                else {
+                    lastDicNode = curNode;
+                }
+                
+            }
+            curNode = nextNode;
+        }
+        if (duplicateFlag == true) {
+            if (lastDicNode != NULL) 
+                lastDicNode->next = NULL;
+            else 
+                head = NULL;
+            delete curNode;
+        }
+        return head;
+    }
+};
+
+#endif
---
+++ 0083.hpp
@@ -0,0 +1,50 @@
+#ifndef __0083_H
+#define __0083_H
+
+/*******************************************
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+Input: 1->1->2
+Output: 1->2
+
+Example 2:
+Input: 1->1->2->3->3
+Output: 1->2->3
+*******************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL)
+            return head;
+        
+        ListNode *preNode=NULL, *curNode=head;
+        while(curNode->next != NULL) {
+            ListNode *nextNode = curNode->next;
+            if (curNode->val == curNode->next->val) {
+                if (preNode != NULL)
+                    preNode->next = curNode->next;
+                else 
+                    head = curNode->next;
+                delete curNode;
+            }
+            else {
+                preNode = curNode;
+            }
+            curNode = nextNode;
+        }
+
+        return head;
+    }
+};
+
+#endif
